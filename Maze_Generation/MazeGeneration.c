7e34fe24dce8d7ddafd7d2fd1c30ea747a3d45ae
Rob Thomas <robedthomas@yahoo.com>
2017-06-01 20:00:25 -0700

    Added structs to MazeGeneration.h and documented them.

---
+++ Maze_Generation/MazeGeneration.c
@@ -0,0 +1,19 @@
+/**
+@file MazeGeneration.c
+@author Rob Thomas
+@brief Contains code for the procedural generation of simple 2D mazes. 
+*/
+
+/*** INCLUDES: ***/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "Queue.h"
+#include "RandomizedQueue.h"
+
+#include "MazeGeneration.h"
+
+/*** FUNCTION DEFINITIONS: ***/
+
